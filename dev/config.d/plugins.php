4d4e678cfcea31067ecdf13d5aaa67f365a79c3a
Welington De Lima Olegario <wolegario49@gmail.com>
2025-06-03 13:55:49 -0300

    Faz a configuração dos plugins

---
+++ dev/config.d/plugins.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'plugins' => [
+        'MultipleLocalAuth' => [ 'namespace' => 'MultipleLocalAuth' ],
+        'SamplePlugin' => ['namespace' => 'SamplePlugin'],
+        'Analytics',
+        'AdminLoginAsUser',
+        'Accessibility',
+        'SpamDetector',
+        'MapasBlame' => [
+            'namespace' => 'MapasBlame',
+            'config' => [
+                'request.logData.PATCH' => function ($data) {
+                    return $data;
+                },
+            ]
+        ],
+    ]
+];
