8008c33a49d38ce7646c99ad06c23dbbd013cb7d
erleibiazzio <erleibiazzio@gmail.com>
2025-06-12 12:38:03 -0300

    Ajusta configuração do letsEncript

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -6,13 +6,12 @@ if ! [ -x "$(command -v docker-compose)" ]; then
 fi
 
 echo "EDITE ESTE ARQUIVO E DEFINA AS VARIÁVEIS domain, email e staging"
-exit; # E APAGUE ESSA LINHA
 
 # Domínio da instalação
-domain=(meumapa.gov.br)
+domain=(mapacultura.novalima.mg.gov.br)
 
 # Informe um e-mail válido
-email="webmaster@meumapa.gov.br"
+email="erlei.biazzio@hacklab.com.br"
 
 # EVITA que se atinja o LIMITE DE REQUESTS ao Let's Encrypt enquanto se testa as configurações
 # defina stagin=0 quando os testes passarem e execute novamente o script
