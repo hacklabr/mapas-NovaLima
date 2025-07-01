c0ff0ce291053a97c8c5759819090d18617a0c41
Welington De Lima Olegario <wolegario49@gmail.com>
2025-07-01 16:41:08 -0300

    Configuração das imagens das oportunidades no ambiente  Ref.: #3

---
+++ dev/config.d/module.home.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    "module.home" => [
+        'home-header' => env('MODULE_HOME_IMAGE_HEADER','img/home/home-principal.png'),
+        'home-agents' => env('MODULE_HOME_IMAGE_AGENT','img/home/Agentes.png'),
+        'home-events' => env('MODULE_HOME_IMAGE_EVENTS','img/home/Eventos.png'),
+        'home-opportunities' => env('MODULE_HOME_IMAGE_OPPORTUNITIES','img/home/Oportunidades.png'),
+        'home-projects' => env('MODULE_HOME_IMAGE_PROJECTS','img/home/Projetos.png'),
+        'home-spaces' => env('MODULE_HOME_IMAGE_SPACES','img/home/Espacos.png'),
+    ]  
+];
