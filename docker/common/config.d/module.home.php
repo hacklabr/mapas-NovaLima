10b2469bf161f6bcad74ef20e782789eb19dfe4b
Welington De Lima Olegario <wolegario49@gmail.com>
2025-07-04 12:25:46 -0300

    Configuração das imagens das oportunidades no ambiente Ref.: #3

--- docker/common/config.d/module.home.php
+++ docker/common/config.d/module.home.php
@@ -6,6 +6,6 @@ return [
         'home-events' => env('MODULE_HOME_IMAGE_EVENTS','img/home/Eventos.png'),
         'home-opportunities' => env('MODULE_HOME_IMAGE_OPPORTUNITIES','img/home/Oportunidades.png'),
         'home-projects' => env('MODULE_HOME_IMAGE_PROJECTS','img/home/Projetos.png'),
-        'home-spaces' => env('MODULE_HOME_IMAGE_SPACES','img/home/Espaços.png'),
+        'home-spaces' => env('MODULE_HOME_IMAGE_SPACES','img/home/Espacos.png'),
     ]  
 ];
