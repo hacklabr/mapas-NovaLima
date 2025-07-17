ef72b1f56829ef9fb77f7ff4c9cdccad34adc0d1
Welington De Lima Olegario <wolegario49@gmail.com>
2025-07-17 11:29:42 -0300

    Implementa estrutura HTML/CSS para os Termos de Uso

---
+++ docker/common/config.d/lgpd.php
@@ -0,0 +1,18 @@
+<?php
+
+use MapasCulturais\i;
+
+return [
+    'module.LGPD' => [
+        'termsOfUsage'=>[
+            'title'=> i::__('Termos de Uso'), 
+            'text'=> file_get_contents(__DIR__ . '/lgpd-terms/terms-of-usage.html'),
+            'buttonText' => i::__('Aceito os termos de uso')
+        ],
+        'privacyPolicy' => [
+            'title'=>  i::__('Política de Privacidade'),
+            'text'=> file_get_contents(__DIR__ . '/lgpd-terms/privacy-policy.html'),
+            'buttonText' => i::__('Aceito as políticas de privacidade')
+        ],
+    ]
+];
